01cb8ae0b9adae69d89915a8ec7cbfe03b30dc42
turgunbaevaa <nyc.aruu@gmail.com>
2024-02-21 15:42:09 +0100

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Aruuke Turgunbaeva on 21/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
